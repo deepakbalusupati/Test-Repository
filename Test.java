ada0b97f3132b295ac4ee7f5a2d0ef48fadc4e57
deepakbalusupati <balusupatideepak@gmail.com>
2024-03-22 12:49:08 +0530

    Program With Sum of Two Integers

--- Test.java
+++ Test.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public void Sum(int a, int b) {
+        int Result = a + b;
+        System.out.println(Result);
+    }
+}
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("I Love You");
+        Scanner scanner = new Scanner(System.in);
+        Main myObj = new Main();
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        myObj.Sum(x, y);
     }
 }
